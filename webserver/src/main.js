7fb8f2b8c940d0326112f4b5ee17220c9267e075
Steven Ceuppens <steven.ceuppens@icloud.com>
2019-04-02 18:15:37 +0200

    added x-frame-options header

--- webserver/src/main.js
+++ webserver/src/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const frameguard = require('frameguard')
 
 const rootapp = express()
 const rootport = 3000
@@ -6,9 +7,14 @@ const rootport = 3000
 const guestapp = express()
 const guestport = 3001
 
+rootapp.use(frameguard({ action: 'deny' }))
 rootapp.use('/', express.static('./rootapp/src/'))
 rootapp.use('/guestapp', express.static('./guestapp/src/'))
 
+guestapp.use(frameguard({
+    action: 'allow-from',
+    domain: 'http://127.0.0.1:3000'
+}));
 guestapp.use('/', express.static('./guestapp/src/'))
 
 rootapp.listen(rootport, () => console.log(`RootApp listening on port ${rootport}!`))
